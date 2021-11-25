6511079bac02481f5ca31ca697bff70943f92983
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2021-11-24 20:34:21 -0800

    Handling some more assets for the models...

--- image_classification/work_with_models.py
+++ image_classification/work_with_models.py
@@ -29,6 +29,14 @@ from string import punctuation
 from .forms import ImageUploadForm
 
 
+def get_tweets(df):
+  return L(df.iloc[i, 0] for i in range(0, df.shape[0]))
+
+def subword(sz):
+    sp = SubwordTokenizer(vocab_sz=sz)
+    sp.setup(txts)
+    return ' '.join(first(sp([txt]))[:40])
+
 def get_tweet_prediction(account, topic):
     #coati: account will be person's name
     
@@ -41,34 +49,61 @@ def get_tweet_prediction(account, topic):
 
     #coati: go through and retool all this
     subs = ['academic-humanities', 'academic-stem', 'anime', 'astrology', 'conservative', 'hippie-spiritual', 'kpop', 'lgbtq', 'liberal', 'sports', 'tech-nerd']
+    
+    print('Loading dataframes...')
     df_eachsub = []
-
     try:
         df_eachsub = torch.load(os.path.join(path_cwd, path_df, 'df_eachsub_tweets.pkl'))
-        return str(len(df_eachsub))
+        print(str(len(df_eachsub)))
     except Exception as e:
-        return e
+        print(e)
+    
+    spacy = WordTokenizer()
+    tkn_eachsub = []
+    for i in range(0, len(subs)):
+        tkn = Tokenizer(spacy)
+        tkn_eachsub.append(tkn)
+    
+    print('Loading txts...')
+    txts_eachsub = []
+    try:
+        txts_eachsub = torch.load(os.path.join(pawh_cwd, path_df, 'txts_eachsub.pkl'))
+        print(str(len(txts_eachsub)))
+    except Exception as e:
+        print(e)
+    #racc: JUST RUN THIS PART ONCE, AFTER THE .PKL IS CREATED, REMOVE IT
+    # for j in range(0, len(subs)):
+    #     print(df_eachsub[j].shape[0])
+    #     txts = L(df_eachsub[j].iloc[i, 0] for i in range(0, df_eachsub[j].shape[0]))
+    #     txts_eachsub.append(txts)
+    # torch.save(txts_eachsub, os.path.join(path_cwd, path_df, 'txts_eachsub.pkl'))
 
 
 
-    # for i in range(0, len(subs)):
-    #     df = None
-    #     newfolder = os.path.join(path_df, subs[i])
-    #     num = 0
-    #     docexists = True
-    #     while docexists == True:
-    #         num += 1
-    #         newpath = os.path.join(newfolder, ('tweets_extracted_' + '{:03d}'.format(num) + '.txt'))
-    #         if newpath.exists():
-    #         print('Reading ' + subs[i] + ' ' + str(num))
-    #         if df is None:
-    #             df = pd.read_csv(newpath, sep='\n', header=None)
-    #             df.columns = ['Tweet']
-    #         else:
-    #             df_new = pd.read_csv(newpath, sep='\n', header=None)
-    #             df_new.columns = ['Tweet']
-    #             df = df.append(df_new)
-    #         else:
-    #         docexists = False
-    #         df_eachsub.append(df)
-    #         print(df.shape)
+
+    #coati: VERIFIED UP TO HERE WORKS...
+
+
+
+
+    print('Loading toks200...')
+    toks200_eachsub = []
+    try:
+        download_toks200()
+        toks200_eachsub = torch.load(os.path.join(path_cwd, path_toks200, 'toks200_tweets.pkl'))
+        print(str(len(toks200_eachsub)))
+    except Exception as e:
+        print(e)
+    
+    print('Loading dataloaders...')
+    dls_eachsub = []
+    try:
+        for i in range(0, len(subs)):
+            filename = 'dls-nlp-' + subs[i] + '-ALT.pkl'
+            dls_thissub = torch.load(os.path.join(pawh_cwd, path_dls, filename))
+            dls_eachsub.append(dls_thissub)
+        print(str(len(dls_eachsub)))
+    except Exception as e:
+        print(e)
+    
+    return 'got to end'
